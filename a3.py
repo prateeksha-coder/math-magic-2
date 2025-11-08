d2ae879a007de8baa890a3e8cb44735b2d6550f1
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-11-08 11:27:59 +0530

    initial commit

--- a3.py
+++ a3.py
@@ -3,10 +3,10 @@ number=int(input("Enter your number \n"))
 if number>1:
   for i in range(2,int(sqrt(number))+1):
     if(number%i==0):
-      print(number, " is not prime")
+      print(number, " is not prime number")
       break
   else:    
-    print(number, " is prime")
+    print(number, " is prime number")
 else:
-  print(number, "is not prime")
+  print(number, "is not prime number")
   
